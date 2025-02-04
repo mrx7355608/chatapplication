fd7c5b27326a3cfee26fcb02c075577065ca3a49
mrx7355608 <mrx7355608@gmail.com>
2025-02-04 22:48:28 +0500

    added pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import ConversationsList from "@/components/ConversationList";
-import VerticalNavbar from "@/components/VerticalNavbar";
 
 export default function Home() {
   return (
     <div className="flex">
-      <VerticalNavbar />
       <ConversationsList />
       <h1>Hello</h1>
     </div>
