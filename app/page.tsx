dedf476345f048d04f25e6ca39e5c5dce03f8c7c
mrx7355608 <mrx7355608@gmail.com>
2025-02-06 17:11:40 +0500

    formatted page.tsx for 4 tabwidth size

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import ConversationsList from "@/components/ConversationList";
 
-export default function Home() {
-  return (
-    <div className="flex">
-      <ConversationsList />
-      <h1>Hello</h1>
-    </div>
-  );
+export default async function Home() {
+    return (
+        <div className="flex">
+            <ConversationsList />
+            <h1>Hello</h1>
+        </div>
+    );
 }
