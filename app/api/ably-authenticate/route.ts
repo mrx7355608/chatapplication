0458b893ffcf790955bb9e27f0f4c362f34b6047
mrx7355608 <mrx7355608@gmail.com>
2025-02-13 07:19:22 +0500

    installed ably realtime

---
+++ app/api/ably-authenticate/route.ts
@@ -0,0 +1,16 @@
+import * as Ably from "ably";
+
+// ensure Vercel doesn't cache the result of this route,
+// as otherwise the token request data will eventually become outdated
+// and we won't be able to authenticate on the client side
+export const revalidate = 0;
+
+export async function GET() {
+    const client = new Ably.Rest({
+        key: process.env.ABLY_API_KEY,
+    });
+    const tokenRequestData = await client.auth.createTokenRequest({
+        clientId: Date.now().toString(),
+    });
+    return Response.json(tokenRequestData);
+}
