ac09b4b48ca593f402b1b40fbe5c4a47d761a5d1
mrx7355608 <mrx7355608@gmail.com>
2025-02-15 18:44:00 +0500

    updated firebase version in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,10 +3,10 @@
 // are not available in the service worker.
 // Replace 10.13.2 with latest version of the Firebase JS SDK.
 importScripts(
-    "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js",
+    "https://www.gstatic.com/firebasejs/11.3.1/firebase-app-compat.js",
 );
 importScripts(
-    "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js",
+    "https://www.gstatic.com/firebasejs/11.3.1/firebase-messaging-compat.js",
 );
 
 // Initialize the Firebase app in the service worker
@@ -14,16 +14,28 @@ try {
     fetch("/api/firebase-data", { credentials: "include" })
         .then((resp) => resp.json())
         .then((config) => {
-            firebase.initializeApp(config);
-            const messaging = firebase.messaging();
+            const app = firebase.initializeApp(config);
+            const messaging = firebase.messaging(app);
             console.log("Firebase initialized");
 
-            // WARNING: Do not remove the below function otherwise you won't receive notifications
             messaging.onBackgroundMessage((payload) => {
-                console.log({ payload });
+                console.log("Received background message: ", payload);
+                const data = payload.data;
+
+                self.registration.showNotification(data.title, {
+                    body: data.body,
+                    icon: data.image,
+                    data: {
+                        url: data.url,
+                    },
+                });
             });
         })
         .catch(console.log);
 } catch (err) {
     console.log(err);
 }
+
+self.addEventListener("notificationclick", (e) => {
+    e.waitUntil(clients.openWindow(e.notification.data.url)); // and here
+});
